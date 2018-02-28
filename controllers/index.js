e4b9d3e9054ec932e1075c299b5bf771da103150
Lillian Chernin <l.smith1654@gmail.com>
2018-02-28 00:49:37 -0600

    add mp3 files

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const twinkleTwinkleViolin = []
+// const Tone = require('tone');
 
+//create a synth and connect it to the master output (your speakers)
+// var synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+// synth.triggerAttackRelease("C4", "8n");
 
 const index = (req, res) => {
   res.render('./index', {
     documentTitle: "play strings",
+    // synth: synth
   })
 }
 
